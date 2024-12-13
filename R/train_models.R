bbdee1a03326db15d8b254166fe8804c37333aa0
Harry <shkdeemail@gmail.com>
2024-12-13 08:26:01 -0600

    Add dat.rda dataset and update documentation

--- R/train_models.R
+++ R/train_models.R
@@ -15,6 +15,48 @@
 #'
 train_models <- function(K,folds,X,A){
 
+  W = cbind(dat$X, dat$S)
+
+  foldid = c(rep_len(1:nfold, length.out = n)[sample(n)],
+             rep_len(1:nfold, length.out = N-n)[sample(N-n)])
+
+  pi.bs = cf.bs(dat$A[1:n], dat$X, nfold, foldid,
+                fit.fun=fit.fun, link = expit,
+                dev.fun = dev.binary.np)
+  # pi.bs$meas
+  mu1.bs = cf.bs(dat$Y[1:n], dat$X, nfold, foldid,
+                 subset = dat$A[1:n]==1,
+                 fit.fun=fit.fun, link = expit,
+                 dev.fun = dev.binary.np)
+  # mu1.bs$meas
+  mu0.bs = cf.bs(dat$Y[1:n], dat$X, nfold, foldid,
+                 subset = dat$A[1:n]==0,
+                 fit.fun=fit.fun, link = expit,
+                 dev.fun = dev.binary.np)
+
+  # SSL models
+  imp.A.bs = cf.bs(dat$A[1:n], W, nfold, foldid)
+  # imp.A.bs$meas
+  # imp.A1Y1.bs = cf.bs(dat$A[1:n]*dat$Y[1:n], W, nfold, foldid)
+  imp.A1Y1.bs = cf.bs(dat$Y[1:n], W, nfold, foldid,
+                      subset = dat$A[1:n]==1,
+                      fit.fun=fit.fun, link = expit,
+                      dev.fun = dev.binary.np)
+  # imp.A1Y1.bs$meas
+  # imp.A0Y1.bs = cf.bs((1-dat$A[1:n])*dat$Y[1:n], W, nfold, foldid)
+  imp.A0Y1.bs = cf.bs(dat$Y[1:n], W, nfold, foldid,
+                      subset = dat$A[1:n]==0,
+                      fit.fun=fit.fun, link = expit,
+                      dev.fun = dev.binary.np)
+
+
+
+
+
+
+
+
+
   X_matrix_raw <-3
   Y_matrix_raw <-4
 
